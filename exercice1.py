70a13c5a7d0a713c3693f9248c9c0cc0cb99f3ed
Soubika <soubika.bisoo@gmail.com>
2021-12-13 11:03:05 +0100

    Premier commit exercice variable

---
+++ exercice1.py
@@ -0,0 +1,58 @@
+# Variable
+
+ma_premiere_varaible = "Salut le monde !"
+print(ma_premiere_varaible)
+ma_premiere_varaible = "J'ai faim !!!"
+print(ma_premiere_varaible)
+
+# String
+
+citation = "Pierre Desproges dit 'Je n\'ai jamais bu du vin à outrance, d\'ailleurs je ne sais même pas où c\'est'"
+print(citation)
+
+nom = "bisoo"
+print(nom.title())
+print(nom.capitalize())
+print(nom.upper())
+
+prenom ="Soubika"
+
+print(nom + " " + prenom)
+
+nom2 = "blabla"
+prenom2 = "bloblo"
+phrase = nom2 + " " + prenom2 + " est vraiment très gentil"
+print(phrase)
+
+print(nom.lstrip())
+print(nom.rstrip())
+print(nom.strip())
+
+
+# Number
+
+def aritmetique(a, b) :
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a/b)
+    print(a%b)
+    print(a**b)
+    
+    
+aritmetique(1,2)
+
+a = 0.1
+b = 0.2
+
+print("{:.2f}".format(a+b))
+
+def aritmetique2(a, b) :
+    print("Le resultat de {} + {} est {}".format(a, b, a+b))
+    print("Le resultat de {} - {} est {}".format(a, b, a-b))
+    print("Le resultat de {} * {} est {}".format(a, b, a*b))
+    print("Le resultat de {} / {} est {}".format(a, b, a/b))
+    print("Le resultat de {} % {} est {}".format(a, b, a%b))
+    print("Le resultat de {} ** {} est {}".format(a, b, a**b))
+    
+aritmetique2(2,3)
